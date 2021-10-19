23ad6b676c309fc0252d35d5a8c5599305d79fa5
jeremyalv <jeremyalvax@gmail.com>
2021-10-19 17:19:22 +0700

    added gatsby image plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Layout from './layout';
 import { title } from '../components/index.module.css'
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
     <main>
       <Layout className={title} pageTitle="Home">
         <p>I'm making this blog through the Gatsby tutorial</p>
+        <StaticImage
+          alt="Mountain Image"
+          src="../assets/Mountain.jpg"
+        />
       </Layout>
     </main>
   ) 
