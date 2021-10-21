0f986c97c2bb5b2556da6427c16338d288b3b2ac
jeremyalv <jeremyalvax@gmail.com>
2021-10-21 12:21:33 +0700

    Enabled data querying using GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "gatsby blog",
+    title: "jere.io",
   },
   plugins: [
     'gatsby-plugin-image',
---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './layout';
+
+const BlogPage = () => {
+    return (
+        <Layout pageTitle="Blog Posts">
+            <p>Posts will go here</p>
+        </Layout>
+    )
+}
+
+export default BlogPage
